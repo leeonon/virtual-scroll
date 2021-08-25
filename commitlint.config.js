fb936743cbe5140afd98ec881cdb485de7e1831b
btea <2356281422@qq.com>
2021-08-25 22:15:43 +0800

    feat: init

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'upd', 'del', 'fix', 'refactor', 'test', 'perf', 'docs', 'style', 'revert', 'chore']
+        ],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never']
+    }
+};
