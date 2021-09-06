914986c2a6aba22452e831aee20d28be60c31c03
btea <2356281422@qq.com>
2021-09-06 10:30:11 +0800

    feat: react滚动进度调整

--- src/react/virtual-scroll.tsx
+++ src/react/virtual-scroll.tsx
@@ -1,46 +1,66 @@
-import React, { ReactElement, useEffect, useRef, useState } from "react"
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import { getStyle } from '../utils/getStyle';
 import styles from './virtualScroll.module.scss';
 
 type dataVal = HTMLElement | number | string;
+type listVal = dataVal[];
 type listOptions = {
-    data: dataVal[],
-    h?: number;
-}
-export default function virtualScroll(props: {options: listOptions}): ReactElement {
-    const {options} = props;
-    const data = options.data;
-    const h = options.h || 30;
-    const sumH = h * data.length;
-    const [base, setBase] = useState(0);
-    const [list, setList] = useState(getShowData(base, data));
-    const bar = useRef(null);
-    const scrollDeal = (eve) => {
-        const scroll_v = eve.target.scrollTop;
-        const per = scroll_v / sumH;
-        const box_h = 400;
-        const translate_y = box_h * per;
-        bar.current.style.transform = `translateY(${translate_y}px)`;
-        const base_num = Math.floor(scroll_v / h);
-        // setBase(base_num);
-        setList(getShowData(base_num, data));
-    }
+	data: listVal;
+	h?: number;
+};
+export default function virtualScroll(props: { options: listOptions }): ReactElement {
+	const { options } = props;
+	const data = options.data;
+	const h = options.h || 30;
+	let sumH = h * data.length;
+	const [base, setBase] = useState(0);
+	const bar = useRef(null);
+	const box = useRef(null);
+	let box_h: number;
+	let bar_h: number;
+	useEffect(() => {
+		const box_h_v = getStyle(box.current, 'height') as string;
+		const bar_h_v = getStyle(bar.current, 'height') as string;
+		box_h = parseInt(box_h_v);
+		bar_h = parseInt(bar_h_v);
+		sumH -= box_h;
+	});
 
-    return <div style={{height: '100%', overflow: "hidden", position: 'relative'}}>
-        <div className={styles["scroll-list-container-box"]} onScroll={scrollDeal}>
-            <ul className={styles["scroll-list-box"]} style={{height: sumH + 'px'}}>
-                {list.map((item,i) => {
-                return <li className={styles["item"]} key={item + '-' + i} style={{height: h + 'px', top: ((item as number) - 1) * h + 'px'}}>{item}</li>
-            })}
-        </ul>
-        </div>
-        <div className={styles["scroll-bar"]} ref={bar}></div>
-    </div>
+	const scrollDeal = (eve: any) => {
+		const scroll_v = eve.target.scrollTop;
+		const per = scroll_v / sumH;
+		const translate_y = (box_h - bar_h) * per;
+		bar.current.style.transform = `translateY(${translate_y}px)`;
+		const base_num = Math.floor(scroll_v / h);
+		setBase(base_num);
+	};
+
+	return (
+		<div style={{ height: '100%', overflow: 'hidden', position: 'relative' }}>
+			<div className={styles['scroll-list-container-box']} onScroll={scrollDeal} ref={box}>
+				<ul className={styles['scroll-list-box']} style={{ height: sumH + 'px' }}>
+					{data.slice(base, base + 20).map((item, i) => {
+						return (
+							<li
+								className={styles['item']}
+								key={item + '-' + i}
+								style={{ height: h + 'px', top: ((item as number) - 1) * h + 'px' }}
+							>
+								{item}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className={styles['scroll-bar']} ref={bar}></div>
+		</div>
+	);
 }
 
-function getShowData(base: number, data: dataVal[]): dataVal[] {
-    base -= base;
-    if (base < 0) {
-        base = 0
-    }
-    return data.slice(base, base + 20);
+function getShowData(base: number, data: listVal): listVal {
+	base -= base;
+	if (base < 0) {
+		base = 0;
+	}
+	return data.slice(base, base + 20);
 }
