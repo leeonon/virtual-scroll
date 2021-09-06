3a74b9201ac3a3bde72a5268f735080fb5a6e4b0
btea <2356281422@qq.com>
2021-09-06 09:56:47 +0800

    feat: vue拖拽滚动

--- src/vue/virtual-scroll.vue
+++ src/vue/virtual-scroll.vue
@@ -7,11 +7,11 @@
 				</li>
 			</ul>
 		</div>
-		<div class="scroll-bar" ref="bar"></div>
+		<div class="scroll-bar" ref="bar" @mousedown="scrollBarDown"></div>
 	</div>
 </template>
 <script lang="ts">
-import { computed, defineComponent, onMounted, reactive, ref } from 'vue';
+import { computed, defineComponent, onBeforeMount, onMounted, reactive, ref } from 'vue';
 import { getStyle } from '../utils/getStyle';
 
 function getShowData(base: number, data: Array<any>): Array<any> {
@@ -44,6 +44,12 @@ export default defineComponent({
 			box_h = parseInt(box_h_v);
 			bar_h = parseInt(bar_h_v);
 			sumHeight.value -= box_h;
+			document.addEventListener('mousemove', docMouseMove);
+			document.addEventListener('mouseup', docMouseUp);
+		});
+		onBeforeMount(() => {
+			document.removeEventListener('mousemove', docMouseMove);
+			document.removeEventListener('mouseup', docMouseUp);
 		});
 		const base = ref(0);
 		const sumHeight = ref(data.length * height);
@@ -63,15 +69,52 @@ export default defineComponent({
 
 		const scrollDeal = (eve: any) => {
 			const scroll_v = eve.target.scrollTop;
-			const per = scroll_v / sumHeight.value;
-			const translate_y = (box_h - bar_h) * per;
-			const el = bar.value! as HTMLElement;
-			el.style.transform = `translateY(${translate_y}px)`;
+			if (isAutoScroll) {
+				const per = scroll_v / sumHeight.value;
+				const translate_y = (box_h - bar_h) * per;
+				const el = bar.value! as HTMLElement;
+				el.style.transform = `translateY(${translate_y}px)`;
+			}
 			const base_num = Math.floor(scroll_v / height);
 			base.value = base_num;
-			// const _list = data.slice(base_num, base_num + 20);
-			// list.values = _list as any;
-			// console.log(list.values);
+		};
+
+		// 拖拽滚动条实现列表一致
+		let isCanMove = false;
+		let initY: number;
+		let translate_y: number = 0;
+		let isAutoScroll = true;
+		const scrollBarDown = (e: MouseEvent) => {
+			isCanMove = true;
+			isAutoScroll = false;
+			initY = e.clientY;
+			const el = bar.value as unknown as HTMLElement;
+			const t_y = el.style.transform;
+			const t_y_Rxp = /translateY\((\d+\.?(\d+)?)px\)/;
+			if (t_y.match(t_y_Rxp)) {
+				translate_y = Number(RegExp.$1);
+			}
+		};
+		const docMouseMove = (e: MouseEvent) => {
+			if (isCanMove) {
+				const diffY = e.clientY - initY;
+				let v = translate_y + diffY;
+				const per = v / (box_h - bar_h);
+				if (v < 0) {
+					v = 0;
+				} else if (v > box_h - bar_h) {
+					v = box_h - bar_h;
+				}
+				const scroll_y = per * sumHeight.value;
+				const box_el = box.value as unknown as HTMLElement;
+				const bar_el = bar.value as unknown as HTMLElement;
+				box_el.scrollTop = scroll_y;
+				bar_el.style.transform = `translateY(${v}px)`;
+			}
+		};
+		const docMouseUp = () => {
+			isCanMove = false;
+			isAutoScroll = true;
 		};
 		return {
 			list,
@@ -82,6 +125,7 @@ export default defineComponent({
 			scrollDeal,
 			bar,
 			box,
+			scrollBarDown,
 		};
 	},
 });
@@ -122,11 +166,13 @@ export default defineComponent({
 		position: absolute;
 		top: 0;
 		right: 0;
+		z-index: 10;
 		width: 10px;
 		height: 30%;
 		border-radius: 10px;
 		background: rgba(102, 204, 255, 0.6);
 		transform: translateY(0);
+		cursor: pointer;
 	}
 }
 </style>
