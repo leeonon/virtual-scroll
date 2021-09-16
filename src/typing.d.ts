00b76dc07600a75068c6138742ff21387e088647
btea <2356281422@qq.com>
2021-09-16 10:29:24 +0800

    type: 类型声明处理

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,25 +1,24 @@
-type boxEle = HTMLElement | string | null
+type boxEle = HTMLElement | string | null;
 
-type dataVal = HTMLElement | number | string
+type dataVal = HTMLElement | number | string;
 
-type style = {
-    [key: string]: number | string
+interface style {
+	[key: string]: number | string | undefined;
 }
+type newStyle = CSSProperties<style>;
 
 type optionsData = {
-    data: dataVal[];
-    h: number
-}
-
+	data: dataVal[];
+	h: number;
+};
 
 type callBack = (e: MouseEvent) => void;
 
-
 declare module '*.module.scss' {
-    const classes: {
-        [key: string]: string;
-    };
-    export default classes;
+	const classes: {
+		[key: string]: string;
+	};
+	export default classes;
 }
 
 declare module '*.vue' {}
